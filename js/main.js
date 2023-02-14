df8dfc5980cb3421c70856dced4c8d13483d4f75
evgenevgen4 <evegjus4@gmail.com>
2023-02-14 06:08:19 +0300

    Made a block popular property with clickable like

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('popular__like')) {
+    e.target.classList.toggle('popular__like--liked')
+  }
+})
+
 // init Swiper:
 const swiper = new Swiper('.swiper', {
   // Optional parameters
@@ -5,6 +11,7 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 5,
   slidesPerGroup: 5,
   spaceBetween: 120,
+  initialSlide: 6,
 
   // If we need pagination
   pagination: {
